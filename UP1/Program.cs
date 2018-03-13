f9bb4f99fb16fbd363bcad836ebf1f27a0cedcc6
jekaklu <jekaklu@gmail.com>
2018-03-13 09:13:54 +0300

    trty

---
+++ UP1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace S4et
+{
+    class Weapon
+    {
+        public String Name { get; set; }
+
+        private double Kaliber { get; set; }
+
+        public double Range { get; set; }
+
+        public Weapon()
+        {
+            this.Name = "";
+            this.Kaliber = 0.0;
+            this.Range = 0.0;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введите название оружия: "); this.Name = Console.ReadLine();
+            Console.Write("Введите калибр оружия: "); this.Kaliber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите его дальность: "); this.Range = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Название: { this.Name }");
+            Console.WriteLine($"Калибр: { this.Kaliber }");
+            Console.WriteLine($"Дальность: { this.Range }");
+        }
+
+        static void Main(string[] args)
+        {
+            Weapon scr = new Weapon();
+            scr.Input();
+            scr.Print();
+            Console.ReadKey();
+        }
+
+    }
+}
